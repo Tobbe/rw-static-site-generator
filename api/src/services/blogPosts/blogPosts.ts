edd79733efb2efb157ecae4a5948f544732b1c55
Tobbe Lundberg <tobbe@tlundberg.com>
2022-07-31 16:04:34 +0200

    DRY blogPosts.ts

--- api/src/services/blogPosts/blogPosts.ts
+++ api/src/services/blogPosts/blogPosts.ts
@@ -11,28 +11,7 @@ interface FrontMatter {
   updateAt?: string
 }
 
-export const blogPosts: QueryResolvers['blogPosts'] = () => {
-  const blogPostFiles = fs
-    .readdirSync('blog/')
-    .filter((filename) => filename.endsWith('.md'))
-
-  const blogPostsList = blogPostFiles.map((fileName) => {
-    const file = fs.readFileSync(`blog/${fileName}`, 'utf-8')
-    const metadata = fm<FrontMatter>(file)
-
-    return {
-      ...metadata.attributes,
-      slug: fileName.slice(0, -3),
-      body: metadata.body,
-    }
-  })
-
-  return blogPostsList.sort(
-    (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
-  )
-}
-
-export const blogPost: QueryResolvers['blogPost'] = ({ slug }) => {
+function getBlogPost(slug: string) {
   if (slug.includes('.') || slug.includes('/') || slug.includes('\\')) {
     throw new Error('Invalid slug ' + slug)
   }
@@ -41,5 +20,23 @@ export const blogPost: QueryResolvers['blogPost'] = ({ slug }) => {
 
   const metadata = fm<FrontMatter>(file)
 
-  return { ...metadata.attributes, body: metadata.body }
+  return { ...metadata.attributes, slug, body: metadata.body }
+}
+
+export const blogPosts: QueryResolvers['blogPosts'] = () => {
+  const blogPostFiles = fs
+    .readdirSync('blog/')
+    .filter((filename) => filename.endsWith('.md'))
+
+  const blogPostsList = blogPostFiles.map((fileName) =>
+    getBlogPost(fileName.slice(0, -3))
+  )
+
+  return blogPostsList.sort(
+    (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+  )
+}
+
+export const blogPost: QueryResolvers['blogPost'] = ({ slug }) => {
+  return getBlogPost(slug)
 }
